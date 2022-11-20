f63576e531d4b91cca7e3b88321b165052bcfe5e
Muhesh Kumar <muhesh4102002@gmail.com>
2022-11-21 01:45:50 +0530

    fix: fix background color of GeneralLayout component not applied to the full screen height

--- src/common/layouts/GeneralPageLayout.tsx
+++ src/common/layouts/GeneralPageLayout.tsx
@@ -6,9 +6,9 @@ import { PageLayoutProps } from 'types/layout';
 
 const GeneralPageLayout: FC<PageLayoutProps> = ({ children }) => {
   return (
-    <div className="h-screen overflow-x-hidden font-openSans relative bg-primaryLight">
+    <div className="h-screen font-openSans relative bg-primaryLight">
       <Navbar />
-      <div className="h-screen px-40 pt-10 bg-background">{children}</div>
+      <div className="px-40 pt-10 bg-background">{children}</div>
     </div>
   );
 };
